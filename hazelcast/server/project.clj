41dbe3e1dd4c4cd1fc76dd37f0e32d7d48c7a391
arodionov <andrii.rodionov@hazelcast.com>
2023-07-26 11:19:54 +0200

    Update Hazelcast to 5.4.0-SNAPSHOT and use JDK 21

--- hazelcast/server/project.clj
+++ hazelcast/server/project.clj
@@ -9,7 +9,7 @@
                  [org.clojure/tools.cli "1.0.214"]
                  [org.clojure/tools.logging "1.2.4"]
                  [spootnik/unilog "0.7.31"]
-                 [com.hazelcast/hazelcast-enterprise "5.3.0"]]
+                 [com.hazelcast/hazelcast-enterprise "5.4.0-SNAPSHOT"]]
   :profiles {:uberjar {:uberjar-name "hazelcast-server.jar"}}
   :main jepsen.hazelcast-server
   :aot [jepsen.hazelcast-server]
